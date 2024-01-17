c8d9a7918ed5018ed6e0daafb1bc15cb15f905a2
서현범 <shb1998@naver.com>
2024-01-17 13:13:43 +0900

    Today I Learned

--- 24_1_16.py
+++ 24_1_16.py
@@ -81,3 +81,121 @@
 
 # print(*array[0][1:3]) #unpacking
 
+
+
+#======================================
+# 2차원 리스트를 입력받는 방법 -> 행, 열이라는 표현 -> matrix[y][x]
+# 1. for문 활용
+# rows = int(input("행의 개수를 입력하세요"))
+# matrix = []
+
+# for i in range(rows):
+#     row = list(map(int, input().split()))
+#     matrix.append(row)
+
+# for row in matrix:
+#     print(row)
+
+# 2. 리스트 컴프리헨션
+# matrix = [list(map(int, input().split())) for _ in range(rows)]
+
+# for row in matrix:
+#     print(row)
+
+
+# 시퀀스 자료형 ---> 리스트, 튜플, range
+# 리스트 : 가변 시퀀스 자료형
+# 튜플 : 불변 시퀀스 자료형
+# range : 불변 시퀀스 자료형
+
+# 튜플 쓰이는 예시 -> 방향배열 알고리즘 문제 
+# -> 여러 값을 그룹화하여 하나의 불변 객체로 만든다.
+
+# def move_around(position):
+#     x, y = position
+#     direct = [(0,1), (0,-1), (1,0), (-1,0)] # 상, 하, 좌, 우
+
+# range 쓰이는 예시 ---> 주로 반복문과 함께 쓰인다
+
+# range(end) : 0부터 end-1까지 1씩 증가
+# ex) for i in range(10): -> 10번 반복하는 반복문(0부터 시작하기 때문)
+
+# range(start, end) : start부터 end-1까지 1씩 증가
+# range(start, end, step)
+# 1. start < end, step > 0 
+# : start부터 end-1까지 step만큼 증가
+
+# 2. start > end, steo < 0
+# : start부터 end+1까지 step만큼 감소
+#=======================================
+
+#<DICT>
+
+# 중복된 키를 사용하면 어떤 value가 해당 키에 적용될까요?
+# 마지막에 할당된 값으로 적용
+
+#딕셔너리 실습
+
+# my_dict = {
+#     'a1' : {'b1' : 1, 'b2' : 2, 'b3' : 3},
+#     'a2' : {'b1' : 4, 'b2' : 5, 'b3' : 6},
+#     'a3' : {'b1' : 7, 'b2' : 8, 'b3' : 9}
+# }
+
+# #value 5를 출력해보세요.
+# value=my_dict['a2']['b2']
+# value = my_dict.get('a2').get('b2')
+
+# print(value)
+
+
+#======================================
+
+# set : 가변 비시퀀스 자료형 -> 중복을 허용하지 x -> 수학의 집합과 동일한 연산 가능
+
+# my_set1 = {1, 2, 3, 4, 5}
+# my_set2 = {3, 4, 5, 6}
+
+# result1 = my_set1 | my_set2
+# result2 = my_set1 - my_set2
+# result3 = my_set1 & my_set2
+
+# print(result1)
+# print(result2)
+# print(result3)
+
+##세트의 사용 예시 : 로또 번호 6개 추출하여 출력하는 프로그램
+
+# import random
+
+# lottery = set()
+# while len(lottery) < 6:
+#     a = random.randint(1,45)
+#     lottery.add(a)
+
+# lottery=list(lottery)
+# lottery.sort()
+
+# print(lottery)
+
+# #sort()는 재귀 못 한다.
+# a=[1,3,4,2,5]
+# a.sort()
+# print(a)
+
+
+#===========================================
+
+#None
+
+# None 과 Null의 차이는 뭘까?
+
+
+#============================================
+
+# n=int(input()) #3.5 입력하면 오류남
+
+# n=3.5
+# int(n)  #3
+
+# 위 아래 다름
