4abd6dabee2e8f90e634b4932644a248577776d4
서현범 <shb1998@naver.com>
2024-03-03 21:31:25 +0900

    240303

--- 알고리즘/개인/A형 대비/농사 로봇.py
+++ 알고리즘/개인/A형 대비/농사 로봇.py
@@ -12,190 +12,190 @@ T  = int(input())
 
 
 
-def mor(arr,s,cur_dir):
-    global cnt
-    days = M
-    next_pos = False
-
-    while days:
-        days -= 1
-        i,j = s
-        di, dj = cur_dir
-        next_pos = False #다음에 갈 수 있는지 여부 갱신
-        # 씨앗 갱신
-        for seed in seed_point:
-            if arr[seed[0]][seed[1]] < 4: # 덜 자란 애들만
-                arr[seed[0]][seed[1]] += 1 # 4일 째 되면 수확물이 다 자란다
-
-        # 다음 방향 조정
-        if dj:
-            dir_cands = [[dj, di], [di, dj], [-dj, di], [-di, -dj]]  # dj가 0이 아닐 때 맞음
-            for d in dir_cands:
-                ni, nj = i + d[0], j + d[1]
-                if (0 <= ni < N and 0 <= nj < N) and (arr[ni][nj] == 0 or arr[ni][nj] == 4): # 범위 내에서 농산물이 있거나, 아무것도 안 심은 농지일 때
-                    next_d = d
-                    next_pos = True
-                    break  # 얜 밖에 있는게 맞다.
-        else:
-            dir_cands = [[dj, -di], [di, dj], [dj, di], [-di, -dj]]
-            for d in dir_cands:
-                ni, nj = i + d[0], j + d[1]
-                if (0 <= ni < N and 0 <= nj < N) and (arr[ni][nj] == 0 or arr[ni][nj] == 4):
-                    next_d = d
-                    next_pos = True
-                    break  # 얜 밖에 있는게 맞다.
-
-        # 현재 위치에 농산물이 없이, 0 일 때
-        if not arr[i][j]:
-            if next_pos: #다음 위치가 존재할 때, 씨앗 심기
-                arr[i][j] = 1 # 씨 심기
-                seed_point.append([i,j])
-                ni,nj = i+next_d[0], j+next_d[1]
-                cur_dir = next_d
-                s = ni,nj
-                continue
-
-
-        else: # 현재 위치가 농산물이 있는 경우
-            if arr[i][j] == 4:
-                arr[i][j] = 0
-                seed_point.remove([i,j])
-                cnt += 1
-                arr[i][j] = 0
-                ni, nj = i + next_d[0], j + next_d[1]
-                cur_dir = next_d
-                s = ni, nj
-                continue
-
-    return cnt
-
-for tc in range(1,T+1):
-    max_v = 0  # 최대값 초기화
-    N,M = map(int,input().split()) # N은 줄 수, M은 일 수
-    arr = [list(map(int,input().split())) for _ in range(N)]
-    dir = [(1,0),(0,-1),(-1,0),(0,1)]
-
-    start_lst = []
-    for i in range(N):
-        for j in range(N):
-            if not arr[i][j]:
-                start_lst.append([i,j])
-
-
-    # print(start_lst)
-
-    max_v = 0
-    cnt_lst = [0]
-    for s in start_lst:
-        for c in range(4):
-            # sum_v = 0
-            # level = 0
-            seed_point = [] # 씨를 심은 장소
-            cur_dir = dir[c]
-            arrcopy = copy.deepcopy(arr)
-            cnt = 0
-            rst = mor(arrcopy,s,cur_dir)
-            # max_v = max(rst,max_v)
-            cnt_lst.append(rst)
-
-    print(f'#{tc} {max(cnt_lst)}')
-#
-# # ** ydir, xdir, face는 직접 그려보면서 왜 이런 값이 나오는지를 확인해보길 바람.
-# # 지금 보는 방향에서 오앞왼뒤 이동했을때의 y offset
-# ydir = [
-#     # 오앞왼뒤
-#     (1, 0, -1, 0),  # 오
-#     (0, -1, 0, 1),  # 앞
-#     (-1, 0, 1, 0),  # 왼
-#     (0, 1, 0, -1)  # 뒤
-# ]
-# # 지금 보는 방향에서 오앞왼뒤 이동했을때의 x offset
-# xdir = [
-#     # 오앞왼뒤
-#     (0, 1, 0, -1),  # 오
-#     (1, 0, -1, 0),  # 앞
-#     (0, -1, 0, 1),  # 왼
-#     (-1, 0, 1, 0)  # 뒤
-# ]
-# # 지금 보는 방향에서 오앞왼뒤 이동하고 나서 보는 방향
-# face = [
-#     # 오앞왼뒤
-#     (3, 0, 1, 2),  # 오
-#     (0, 1, 2, 3),  # 앞
-#     (1, 2, 3, 0),  # 왼
-#     (2, 3, 0, 1)  # 뒤
-# ]
-#
-#
-# def sim(y, x, dir):
+# def mor(arr,s,cur_dir):
+#     global cnt
 #     days = M
-#     cnt = 0
-#     seedcnt = [[0 for _ in range(N)] for _ in range(N)]  # seedcnt : 각 (y,x) 좌표에 몇번째 (K) 씨앗이 심어졌는지 기록
-#     seeds = []  # 씨앗들
+#     next_pos = False
 #
-#     # M일동안 simulation
-#     while days > 0:
-#         harvested = False
-#         days -= 1  # 하루가 지났다
-#         size = len(seeds)
-#         # 모든 씨앗들 업데이트
-#         for _ in range(size):
-#             now = seeds.pop(0)  # 맨 앞에거 빼고
-#             now[2] += 1  # 날짜 하루 추가
-#             # K + 3일이 지났다면
-#             if now[2] == (3 + seedcnt[now[0]][now[1]]): #  3일 + K(심은 날짜)
-#                 mapcopy[now[0]][now[1]] = 3  # 수확 가능하도록 설정
-#             else:
-#                 seeds.append(now)  # 아니면 다시 queue(list)에 삽입
-#         # 지금 위치가 수확이 가능하다면
-#         if mapcopy[y][x] == 3:
-#             cnt += 1  # 수확 + 1
-#             mapcopy[y][x] = 0  # 빈 땅으로 변경
-#             harvested = True  # 이번 턴에 수확했다! 기록
-#         # 다음 위치 / 방향 확인
-#         nextdir = -1 # 이게 다음 방향의 디폴트값. 불가능할 때 -1
-#         for i in range(4):
-#             ny = y + ydir[dir][i]
-#             nx = x + xdir[dir][i]
-#             # 산이거나, 싹이 튼곳이면 못간다. (테두리는 모두 1이니 범위 검사는 필요 없음)
-#             if mapcopy[ny][nx] == 1 or mapcopy[ny][nx] == 2: # 싹이 튼 곳의 mapcopy는 2
+#     while days:
+#         days -= 1
+#         i,j = s
+#         di, dj = cur_dir
+#         next_pos = False #다음에 갈 수 있는지 여부 갱신
+#         # 씨앗 갱신
+#         for seed in seed_point:
+#             if arr[seed[0]][seed[1]] < 4: # 덜 자란 애들만
+#                 arr[seed[0]][seed[1]] += 1 # 4일 째 되면 수확물이 다 자란다
+#
+#         # 다음 방향 조정
+#         if dj:
+#             dir_cands = [[dj, di], [di, dj], [-dj, di], [-di, -dj]]  # dj가 0이 아닐 때 맞음
+#             for d in dir_cands:
+#                 ni, nj = i + d[0], j + d[1]
+#                 if (0 <= ni < N and 0 <= nj < N) and (arr[ni][nj] == 0 or arr[ni][nj] == 4): # 범위 내에서 농산물이 있거나, 아무것도 안 심은 농지일 때
+#                     next_d = d
+#                     next_pos = True
+#                     break  # 얜 밖에 있는게 맞다.
+#         else:
+#             dir_cands = [[dj, -di], [di, dj], [dj, di], [-di, -dj]]
+#             for d in dir_cands:
+#                 ni, nj = i + d[0], j + d[1]
+#                 if (0 <= ni < N and 0 <= nj < N) and (arr[ni][nj] == 0 or arr[ni][nj] == 4):
+#                     next_d = d
+#                     next_pos = True
+#                     break  # 얜 밖에 있는게 맞다.
+#
+#         # 현재 위치에 농산물이 없이, 0 일 때
+#         if not arr[i][j]:
+#             if next_pos: #다음 위치가 존재할 때, 씨앗 심기
+#                 arr[i][j] = 1 # 씨 심기
+#                 seed_point.append([i,j])
+#                 ni,nj = i+next_d[0], j+next_d[1]
+#                 cur_dir = next_d
+#                 s = ni,nj
 #                 continue
-#             # 오앞왼뒤 우선순위로 갈수 있는곳 찾았으면 break
-#             nextdir = i
-#             break
-#         # 현재 이동이 가능하지 않다면 -> 다음날로
-#         if nextdir == -1:
-#             continue
-#             # 이동이 가능하고, 현재 위치가 빈 땅이면서, 여기서 오늘 수확한곳이 아니라면
-#         if mapcopy[y][x] == 0 and harvested == False:
-#             mapcopy[y][x] = 2  # 씨앗을 뿌리고
-#             seedcnt[y][x] += 1  # 여기에 씨앗을 뿌린 횟수 늘려주고 (K)
-#             seeds.append([y, x, -1])  # 씨앗 추가 (y좌표, x좌표, 날짜)
-#         y = y + ydir[dir][nextdir]  # 다음 Y
-#         x = x + xdir[dir][nextdir]  # 다음 X
-#         dir = face[dir][nextdir]  # 다음 방향
+#
+#
+#         else: # 현재 위치가 농산물이 있는 경우
+#             if arr[i][j] == 4:
+#                 arr[i][j] = 0
+#                 seed_point.remove([i,j])
+#                 cnt += 1
+#                 arr[i][j] = 0
+#                 ni, nj = i + next_d[0], j + next_d[1]
+#                 cur_dir = next_d
+#                 s = ni, nj
+#                 continue
+#
 #     return cnt
 #
+# for tc in range(1,T+1):
+#     max_v = 0  # 최대값 초기화
+#     N,M = map(int,input().split()) # N은 줄 수, M은 일 수
+#     arr = [list(map(int,input().split())) for _ in range(N)]
+#     dir = [(1,0),(0,-1),(-1,0),(0,1)]
 #
-# T = int(input())
-# for tc in range(1, T + 1):
-#     # input
-#     N, M = list(map(int, input().split()))
-#     MAP = [list(map(int, input().split())) for _ in range(N)]
-#     ans = 0
-#     # solve - 모든 위치에서 모든 방향으로 두기
+#     start_lst = []
 #     for i in range(N):
 #         for j in range(N):
-#             # 산이라면 pass
-#             if MAP[i][j] == 1:
-#                 continue
-#             for d in range(4):
-#                 # 돌릴때마다 원본 맵으로 복구 -> mapcopy를 simulation용 맵으로 사용
-#                 mapcopy = copy.deepcopy(MAP)
-#                 temp = sim(i, j, d)
-#                 ans = max(temp, ans)
-#     # output
-#     print(f"#{tc} {ans}")
+#             if not arr[i][j]:
+#                 start_lst.append([i,j])
+#
+#
+#     # print(start_lst)
+#
+#     max_v = 0
+#     cnt_lst = [0]
+#     for s in start_lst:
+#         for c in range(4):
+#             # sum_v = 0
+#             # level = 0
+#             seed_point = [] # 씨를 심은 장소
+#             cur_dir = dir[c]
+#             arrcopy = copy.deepcopy(arr)
+#             cnt = 0
+#             rst = mor(arrcopy,s,cur_dir)
+#             # max_v = max(rst,max_v)
+#             cnt_lst.append(rst)
+#
+#     print(f'#{tc} {max(cnt_lst)}')
+
+# ** ydir, xdir, face는 직접 그려보면서 왜 이런 값이 나오는지를 확인해보길 바람.
+# 지금 보는 방향에서 오앞왼뒤 이동했을때의 y offset
+ydir = [
+    # 오앞왼뒤
+    (1, 0, -1, 0),  # 오
+    (0, -1, 0, 1),  # 앞
+    (-1, 0, 1, 0),  # 왼
+    (0, 1, 0, -1)  # 뒤
+]
+# 지금 보는 방향에서 오앞왼뒤 이동했을때의 x offset
+xdir = [
+    # 오앞왼뒤
+    (0, 1, 0, -1),  # 오
+    (1, 0, -1, 0),  # 앞
+    (0, -1, 0, 1),  # 왼
+    (-1, 0, 1, 0)  # 뒤
+]
+# 지금 보는 방향에서 오앞왼뒤 이동하고 나서 보는 방향
+face = [
+    # 오앞왼뒤
+    (3, 0, 1, 2),  # 오
+    (0, 1, 2, 3),  # 앞
+    (1, 2, 3, 0),  # 왼
+    (2, 3, 0, 1)  # 뒤
+]
+
+
+def sim(y, x, dir):
+    days = M
+    cnt = 0
+    seedcnt = [[0 for _ in range(N)] for _ in range(N)]  # seedcnt : 각 (y,x) 좌표에 몇번째 (K) 씨앗이 심어졌는지 기록
+    seeds = []  # 씨앗들
+
+    # M일동안 simulation
+    while days > 0:
+        harvested = False
+        days -= 1  # 하루가 지났다
+        size = len(seeds)
+        # 모든 씨앗들 업데이트
+        for _ in range(size):
+            now = seeds.pop(0)  # 맨 앞에거 빼고
+            now[2] += 1  # 날짜 하루 추가
+            # K + 3일이 지났다면
+            if now[2] == (3 + seedcnt[now[0]][now[1]]): #  3일 + K(심은 날짜)
+                mapcopy[now[0]][now[1]] = 3  # 수확 가능하도록 설정
+            else:
+                seeds.append(now)  # 아니면 다시 queue(list)에 삽입
+        # 지금 위치가 수확이 가능하다면
+        if mapcopy[y][x] == 3:
+            cnt += 1  # 수확 + 1
+            mapcopy[y][x] = 0  # 빈 땅으로 변경
+            harvested = True  # 이번 턴에 수확했다! 기록
+        # 다음 위치 / 방향 확인
+        nextdir = -1 # 이게 다음 방향의 디폴트값. 불가능할 때 -1
+        for i in range(4):
+            ny = y + ydir[dir][i]
+            nx = x + xdir[dir][i]
+            # 산이거나, 싹이 튼곳이면 못간다. (테두리는 모두 1이니 범위 검사는 필요 없음)
+            if mapcopy[ny][nx] == 1 or mapcopy[ny][nx] == 2: # 싹이 튼 곳의 mapcopy는 2
+                continue
+            # 오앞왼뒤 우선순위로 갈수 있는곳 찾았으면 break
+            nextdir = i
+            break
+        # 현재 이동이 가능하지 않다면 -> 다음날로
+        if nextdir == -1:
+            continue
+            # 이동이 가능하고, 현재 위치가 빈 땅이면서, 여기서 오늘 수확한곳이 아니라면
+        if mapcopy[y][x] == 0 and harvested == False:
+            mapcopy[y][x] = 2  # 씨앗을 뿌리고
+            seedcnt[y][x] += 1  # 여기에 씨앗을 뿌린 횟수 늘려주고 (K)
+            seeds.append([y, x, -1])  # 씨앗 추가 (y좌표, x좌표, 날짜)
+        y = y + ydir[dir][nextdir]  # 다음 Y
+        x = x + xdir[dir][nextdir]  # 다음 X
+        dir = face[dir][nextdir]  # 다음 방향
+    return cnt
+
+import copy
+T = int(input())
+for tc in range(1, T + 1):
+    # input
+    N, M = list(map(int, input().split()))
+    MAP = [list(map(int, input().split())) for _ in range(N)]
+    ans = 0
+    # solve - 모든 위치에서 모든 방향으로 두기
+    for i in range(N):
+        for j in range(N):
+            # 산이라면 pass
+            if MAP[i][j] == 1:
+                continue
+            for d in range(4):
+                # 돌릴때마다 원본 맵으로 복구 -> mapcopy를 simulation용 맵으로 사용
+                mapcopy = copy.deepcopy(MAP)
+                temp = sim(i, j, d)
+                ans = max(temp, ans)
+    # output
+    print(f"#{tc} {ans}")
 
 
 
--- 알고리즘/개인/A형 대비/산악 구조.py
+++ 알고리즘/개인/A형 대비/산악 구조.py
@@ -1,64 +1,148 @@
-T = int(input())
+# T = int(input())
 # todo
 # 백트래킹이 아니라 bfs로 que에 이전 노드 넣어서
-def mt(s,cnt=0):
+# def mt(s,cnt=0):
+#     global min_v
+#     i,j = s
+#
+#     if cnt > min_v:
+#         return
+#
+#     if s == e:
+#         min_v = min(cnt,min_v)
+#         return
+#
+#     if s in turner_in: # todo 터널 지나가는 조건 확인
+#         ni,nj = turner[s][:2]
+#         vst.append((ni,nj))
+#         mt((ni,nj),cnt+turner[s][2])
+#         vst.remove((ni,nj))
+#
+#     for d in dir:
+#         ni,nj = i+d[0],j+d[1]
+#         if 0<=ni<N and 0<=nj<N:
+#             if (ni,nj) not in vst:
+#                 vst.append((ni,nj))
+#                 if mnt[ni][nj] > mnt[i][j]:
+#                     tmp = 2*(mnt[ni][nj]-mnt[i][j])
+#                 elif mnt[ni][nj] == mnt[i][j]:
+#                     tmp = 1
+#                 else :
+#                     tmp = 0
+#                 mt((ni,nj),cnt+tmp)
+#                 vst.remove((ni,nj))
+
+from collections import deque as dq
+
+T = int(input())
+def bfs(s,e):
     global min_v
     i,j = s
+    # vst = []
+    q = dq([(i,j)])
+    # print(q)
+    while q:
+        now = q.popleft()
+        # print(now)
+        i,j = now
+        cnt = vst[i][j]
 
-    if cnt > min_v:
-        return
+        if now == e:
+            min_v = min(cnt,min_v)
+            continue
 
-    if s == e:
-        min_v = min(cnt,min_v)
-        return
+        if vst[i][j] >= min_v:
+            continue
 
-    if s in turner_in: # todo 터널 지나가는 조건 확인
-        ni,nj = turner[s][:2]
-        vst.append((ni,nj))
-        mt((ni,nj),cnt+turner[s][2])
-        vst.remove((ni,nj))
+        for d in dir:
+            ni,nj = i+d[0],j+d[1]
+            if 0<=ni<N and 0<= nj <N:
 
-    for d in dir:
-        ni,nj = i+d[0],j+d[1]
-        if 0<=ni<N and 0<=nj<N:
-            if (ni,nj) not in vst:
-                vst.append((ni,nj))
+                # 필요 연료 확인
                 if mnt[ni][nj] > mnt[i][j]:
-                    tmp = 2*(mnt[ni][nj]-mnt[i][j])
+                    tmp = 2 * (mnt[ni][nj] - mnt[i][j])
+
                 elif mnt[ni][nj] == mnt[i][j]:
                     tmp = 1
-                else :
+
+                elif mnt[ni][nj]<mnt[i][j]:
                     tmp = 0
-                mt((ni,nj),cnt+tmp)
-                vst.remove((ni,nj))
+
+                # vst로 가지치기
+                if vst[ni][nj]:#값이 있을 떄
+
+                    if cnt+tmp >= vst[ni][nj]: #만약 현재 값이 기존 값보다 크면
+                        continue
+                    else: # 더 적은 연료를 찾은 경로를 찾으면
+                        vst[ni][nj] = cnt+tmp
+                        next = (ni,nj)
+                        q.append(next)
+                else: # 들른 적 없을 떄
+                    vst[ni][nj] = cnt + tmp
+                    next = (ni, nj)
+                    q.append(next)
+
+
+        for num in tunnel.keys():
+            if (i, j) == tunnel[num][0:2]:
+                out_i, out_j = tunnel[num][2:4]
+                fuel = tunnel[num][4]
+
+                if vst[out_i][out_j]:
+                    if vst[out_i][out_j] > fuel+cnt:
+                        vst[out_i][out_j] = fuel+cnt
+                        q.append((out_i,out_j))
+                    else:
+                        continue
+                else: # 도달한 적 없으면
+                    vst[out_i][out_j] = fuel + cnt
+                    q.append((out_i,out_j))
+                    continue
+
+                # continue
+            elif (i,j) == tunnel[num][2:4]:
+                out_i, out_j = tunnel[num][0:2]
+                fuel = tunnel[num][4]
+                if vst[out_i][out_j]:
+                    if vst[out_i][out_j] > fuel + cnt:
+                        vst[out_i][out_j] = fuel + cnt
+                        q.append((out_i, out_j))
+                    else:
+                        continue
+                else:
+                    vst[out_i][out_j] = fuel + cnt
+                    q.append((out_i, out_j))
+
+                continue
+    # print(vst)
+
+
 
 for tc in range(1,T+1):
     N,M = map(int,input().split())
     mnt = [list(map(int,input().split())) for _ in range(N)]
-    turner = {}
-    for _ in range(M):
+    tunnel = {}
+
+
+    for num in range(M):
         ai,aj,bi,bj,fuel = map(int,input().split())
         ai -= 1
         aj -= 1
         bi -= 1
         bj -= 1
 
-        turner[(ai,aj)] = (bi,bj,fuel)
-        turner[(bi,bj)] = (ai,aj,fuel)
+        tunnel[num] = ai,aj,bi,bj,fuel
+
 # dir을 뒤로도 갈 수 있나?
 
-    turner_in = list(turner.keys())
-    # print(turner)
-    # print(turner_in)
-    cnt_lst = []
-    dir = [(1,0),(0,1)]
+
+    dir = [(1,0),(0,1),(-1,0),(0,-1)]
 
     min_v = float('inf')
     s = (0,0)
     e = (N-1,N-1)
+    vst = [[0]*N for _ in range(N)]
+    vst[0][0] = 1
+    bfs(s,e)
 
-    vst = [s]
-
-    mt(s)
-
-    print(f'#{tc} {min_v}')
+    print(f'#{tc} {min_v-1}')
