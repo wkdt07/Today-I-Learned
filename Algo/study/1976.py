0670f64f08448e0a9317e1fd7bf72c0c5168f137
SHB <shb1998@naver.com>
2024-04-01 23:41:51 +0900

    shb

--- Algo/study/1976.py
+++ Algo/study/1976.py
@@ -10,7 +10,9 @@ arr = [[0]+list(map(int, input().split())) for _ in range(N)]
 
 relation = [[] for _ in range(N+1)]
 
+arr = [[0]*(N+1)] + arr
 
+# print(arr)
 
 parents = list(range(N+1))
 
@@ -31,8 +33,8 @@ def union_set(x, y):
     else:
         parents[y] = x
 
-for i in range(N):
-    for j in range(N):
+for i in range(1,N+1):
+    for j in range(1,N+1):
         if arr[i][j]:
             union_set(i, j)
 
@@ -47,8 +49,10 @@ while i < M:
         break
     i += 1
 
-
+# print(parents)
 if ans:
     print('YES')
 else: print("NO")
+
+
 
