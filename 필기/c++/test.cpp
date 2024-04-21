e534e51f9d1a3c8c738dfb474dcd9e5d5a0e00d3
SHB <shb1998@naver.com>
2024-04-21 22:05:03 +0900

    shb

---
+++ 필기/c++/test.cpp
@@ -0,0 +1,15 @@
+//
+// Created by user on 24. 4. 21.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    char d[21];
+    scanf("%s",d);
+    for(int i=0;d[i]!='\0';i++){
+        printf("\'%c\'\n",d[i]);
+    }
+    return 0;
+}
